12df7dd60788e0938d0097b5d5747209e676eea9
Danil Galeev <galoffdan@gmail.com>
2021-05-05 13:13:21 +0300

    Write binary search

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
 
-int main(int argc, char **argv) {
-  std::cout << "Hello, stranger!" << std::endl;
-  return 0;
+using namespace std;
+// returns index of element in array. If array doesn't have this element, returns -1
+int binary_search(vector<int> arr, int num) {
+    int begin = 0;
+    int end = arr.size();
+    int curr_index = (end + begin) / 2;
+    if (arr[0] == num) {
+        return 0;
+    }
+    if (arr[arr.size() - 1] == num) {
+        return arr.size() - 1;
+    }
+    do {
+        if (arr[curr_index] == num) {
+            return curr_index;
+        }
+        if (arr[curr_index] > num) {
+            end = curr_index;
+        } else {
+            begin = curr_index;
+        }
+        curr_index = (end + begin) / 2;
+    } while (begin != curr_index);
+    return -1;
 }
+int main(int argc, char **argv) {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    cout << binary_search(arr, 2);
+    return 0;
+}
+
